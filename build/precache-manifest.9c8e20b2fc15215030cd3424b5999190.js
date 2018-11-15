66678f66c76199be660a6521990afcf804e7d1f4
Timothy Robards <timothyrobards@gmail.com>
2018-11-15 11:45:54 -0500

    Initial commit of build folder

---
+++ build/precache-manifest.9c8e20b2fc15215030cd3424b5999190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "d8eaffb9a156b1baa619",
+    "url": "/myportfolio/static/js/main.d8eaffb9.chunk.js"
+  },
+  {
+    "revision": "0a5ae4189c0e79ebbf49",
+    "url": "/myportfolio/static/js/1.0a5ae418.chunk.js"
+  },
+  {
+    "revision": "d8eaffb9a156b1baa619",
+    "url": "/myportfolio/static/css/main.153b20f6.chunk.css"
+  },
+  {
+    "revision": "0a5ae4189c0e79ebbf49",
+    "url": "/myportfolio/static/css/1.24d4b812.chunk.css"
+  },
+  {
+    "revision": "52a6b66d8a3d9168be6739f32063de1b",
+    "url": "/myportfolio/index.html"
+  }
+];
